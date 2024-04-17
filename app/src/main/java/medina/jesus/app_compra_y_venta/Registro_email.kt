3fe56bab82c380da9659daf54aa6313518caeae0
JesusMedinaP <jesusmedinaparraga@gmail.com>
2024-04-17 15:39:12 +0200

    Cuarta Parte del Inicio de sesión y Registro con Firebase. Creada la vista de registro y la funcionalidad para poder acceder a ella.

---
+++ app/src/main/java/medina/jesus/app_compra_y_venta/Registro_email.kt
@@ -0,0 +1,11 @@
+package medina.jesus.app_compra_y_venta
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Registro_email : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro_email)
+    }
+}
--- app/src/main/java/medina/jesus/app_compra_y_venta/opciones_login/Login_email.kt
+++ app/src/main/java/medina/jesus/app_compra_y_venta/opciones_login/Login_email.kt
@@ -1,12 +1,22 @@
 package medina.jesus.app_compra_y_venta.opciones_login
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import medina.jesus.app_compra_y_venta.R
+import medina.jesus.app_compra_y_venta.Registro_email
+import medina.jesus.app_compra_y_venta.databinding.ActivityLoginEmailBinding
 
 class Login_email : AppCompatActivity() {
+
+    private lateinit var binding : ActivityLoginEmailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_email)
+        binding = ActivityLoginEmailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.TxtRegistrarme.setOnClickListener {
+            startActivity(Intent(this@Login_email, Registro_email::class.java))
+        }
     }
 }
