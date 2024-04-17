45aba3a4fe55fa29920e18cf881653d53370e6b1
JesusMedinaP <jesusmedinaparraga@gmail.com>
2024-04-17 14:37:46 +0200

    Primera Parte del Inicio de sesión y Registro con Firebase. Creada la vista para poder ingresar con Email o Con Google, añadidos los iconos y cambiado el manifest para que la vista principal sea esa. Añadido el uso de Internet para nuestra aplicación al manifest

---
+++ app/src/main/java/medina/jesus/app_compra_y_venta/OpcionesLogin.kt
@@ -0,0 +1,14 @@
+package medina.jesus.app_compra_y_venta
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class OpcionesLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_opciones_login)
+    }
+}
