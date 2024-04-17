c89859fa74eebeaf3559e0c5ee2874af7155e056
JesusMedinaP <jesusmedinaparraga@gmail.com>
2024-04-17 12:15:39 +0200

    Terminada la segunda parte del BottonNavigationMenú. En resumen, implementado un listener para cada icono de la barra de navegacióny la  lógica para cambiar de fragmento al pulsarlos. También la carpeta que contendrá los fragmentos para cada vista asociado a esos iconos.

--- app/src/main/java/medina/jesus/app_compra_y_venta/MainActivity.kt
+++ app/src/main/java/medina/jesus/app_compra_y_venta/MainActivity.kt
@@ -5,6 +5,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ReportFragment.Companion.reportFragment
+import medina.jesus.app_compra_y_venta.Fragmentos.FragmentChats
+import medina.jesus.app_compra_y_venta.Fragmentos.FragmentCuenta
+import medina.jesus.app_compra_y_venta.Fragmentos.FragmentInicio
+import medina.jesus.app_compra_y_venta.Fragmentos.FragmentMisAnuncios
 import medina.jesus.app_compra_y_venta.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,7 +19,75 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
+
+        verFragmentInicio()
+
+        binding.BottomNV.setOnItemSelectedListener {item->
+            when(item.itemId)
+            {
+                R.id.Item_Inicio->{
+                    verFragmentInicio()
+                    true
+                }
+
+                R.id.Item_Chats->
+                {
+                    verFragmentChats()
+                    true
+                }
+
+                R.id.Item_Mis_Anuncios->{
+                    verFragmentMisAnuncios()
+                    true
+                }
+
+                R.id.Item_Cuenta->
+                {
+                    verFragmentCuenta()
+                    true
+                }
+
+                else->{
+                    false
+                }
+            }
+        }
+    }
+
+    private fun verFragmentInicio()
+    {
+        binding.TituloRL.text = "Inicio"
+        val fragment = FragmentInicio()
+        val fragmentTransition = supportFragmentManager.beginTransaction()
+        fragmentTransition.replace(binding.FragmentL1.id, fragment, "FragmentInicio")
+        fragmentTransition.commit()
+    }
+
+    private fun verFragmentChats()
+    {
+        binding.TituloRL.text = "Chats"
+        val fragment = FragmentChats()
+        val fragmentTransition = supportFragmentManager.beginTransaction()
+        fragmentTransition.replace(binding.FragmentL1.id, fragment, "FragmentChats")
+        fragmentTransition.commit()
+    }
+
+    private fun verFragmentMisAnuncios()
+    {
+        binding.TituloRL.text = "Mis Anuncios"
+        val fragment = FragmentMisAnuncios()
+        val fragmentTransition = supportFragmentManager.beginTransaction()
+        fragmentTransition.replace(binding.FragmentL1.id, fragment, "FragmentMisAnuncios")
+        fragmentTransition.commit()
+    }
+
+    private fun verFragmentCuenta()
+    {
+        binding.TituloRL.text = "Cuenta"
+        val fragment = FragmentCuenta()
+        val fragmentTransition = supportFragmentManager.beginTransaction()
+        fragmentTransition.replace(binding.FragmentL1.id, fragment, "FragmentCuenta")
+        fragmentTransition.commit()
     }
 }
