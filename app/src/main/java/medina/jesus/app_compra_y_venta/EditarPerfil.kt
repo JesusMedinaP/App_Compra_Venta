e574ac0b4551e3fae1cfb3702327dbfd47593d1e
JesusMedinaP <jesusmedinaparraga@gmail.com>
2024-04-22 12:42:31 +0200

    Segunda Parte de Perfil Usuario. Añadida la lógica para leer la información de la vista Editar Perfil en tiempo real.

--- app/src/main/java/medina/jesus/app_compra_y_venta/EditarPerfil.kt
+++ app/src/main/java/medina/jesus/app_compra_y_venta/EditarPerfil.kt
@@ -1,20 +1,86 @@
 package medina.jesus.app_compra_y_venta
 
+import android.app.ProgressDialog
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import medina.jesus.app_compra_y_venta.databinding.ActivityEditarPerfilBinding
 
 class EditarPerfil : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEditarPerfilBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var progressDialog: ProgressDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_editar_perfil)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityEditarPerfilBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Espere por favor")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        cargarInfo()
+    }
+
+    private fun cargarInfo() {
+        val ref = FirebaseDatabase.getInstance(Constantes.REFERENCIADB).getReference("Usuarios")
+        ref.child("${firebaseAuth.uid}")
+            .addValueEventListener(object: ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val nombres = "${snapshot.child("nombres").value}"
+                    val imagen = "${snapshot.child("urlImagenPerfil").value}"
+                    val f_nacimiento = "${snapshot.child("fecha_nac").value}"
+                    val telefono = "${snapshot.child("telefono").value}"
+                    val codTelefono = "${snapshot.child("codigoTelefono").value}"
+
+                    //Volcamos la información en las vistas.
+
+                    binding.EtNombres.setText(nombres)
+                    binding.EtFechaNacimiento.setText(f_nacimiento)
+                    binding.EtTelefono.setText(telefono)
+
+                    try {
+
+                        Glide.with(applicationContext)
+                            .load(imagen)
+                            .placeholder(R.drawable.img_perfil)
+                            .into(binding.imgPerfil)
+                    }catch (e: Exception){
+                        Toast.makeText(
+                            this@EditarPerfil,
+                            "${e.message}",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+
+                    //Transformación del código del país sin el +
+                    try {
+                        if (codTelefono != "")
+                        {
+                            val codigo = codTelefono.replace("+", "").toInt()
+                            binding.selectorCod.setCountryForPhoneCode(codigo)
+                        }
+                    }catch (e: Exception){
+                        Toast.makeText(
+                            this@EditarPerfil,
+                            "${e.message}",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                }
+
+            })
     }
 }
