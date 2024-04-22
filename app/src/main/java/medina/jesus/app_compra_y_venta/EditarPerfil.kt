7c6ee407a5e63ff3788a3263261fb4fca230dea7
JesusMedinaP <jesusmedinaparraga@gmail.com>
2024-04-22 13:21:03 +0200

    Cuarta Parte de Perfil Usuario. Implementada la funcionalidad para que la aplicación pueda acceder tanto a la cámara como a la galería y pueda establecerse la imagen de perfil. Añadidos los permisos para acceder tanto a la cámara como al almacenamiento interno

--- app/src/main/java/medina/jesus/app_compra_y_venta/EditarPerfil.kt
+++ app/src/main/java/medina/jesus/app_compra_y_venta/EditarPerfil.kt
@@ -1,10 +1,18 @@
 package medina.jesus.app_compra_y_venta
 
+import android.app.Activity
 import android.app.ProgressDialog
+import android.content.ContentResolver
+import android.content.ContentValues
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.Menu
 import android.widget.PopupMenu
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
@@ -19,6 +27,8 @@ class EditarPerfil : AppCompatActivity() {
     private lateinit var binding: ActivityEditarPerfilBinding
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var progressDialog: ProgressDialog
+
+    private var imageUri : Uri ?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -104,12 +114,129 @@ class EditarPerfil : AppCompatActivity() {
             if(itemId == 1)
             {
                 // Cámara
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
+                {
+                    concederPermisoCamara.launch(arrayOf(android.Manifest.permission.CAMERA))
+                }else{
+                    concederPermisoCamara.launch(arrayOf(
+                        android.Manifest.permission.CAMERA,
+                        android.Manifest.permission.WRITE_EXTERNAL_STORAGE))
+                }
             }else if(itemId == 2)
             {
-                //Galería
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
+                {
+                    imagenGaleria()
+                }else{
+                    concederPermisoAlmacenamiento.launch(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                }
             }
             return@setOnMenuItemClickListener true
         }
 
     }
+
+    private val concederPermisoCamara =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){
+            resultado ->
+            var concedidoTodos = true
+            for(concesion in resultado.values)
+            {
+                concedidoTodos = concedidoTodos && concesion
+            }
+
+            if(concedidoTodos){
+                imagenCamara()
+            }else{
+                Toast.makeText(
+                    this,
+                    "El permiso de la cámara o almacenamiento ha sido denegado",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+    private fun imagenCamara() {
+        val  contentValues = ContentValues()
+        contentValues.put(MediaStore.Images.Media.TITLE, "Titulo_imagen")
+        contentValues.put(MediaStore.Images.Media.DESCRIPTION, "Descripcion_imagen")
+        imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+        resultadoCamara_ARL.launch(intent)
+    }
+
+    private val resultadoCamara_ARL =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+        {
+            resultado ->
+            if(resultado.resultCode == Activity.RESULT_OK)
+            {
+                try{
+                    Glide.with(this)
+                        .load(imageUri)
+                        .placeholder(R.drawable.img_perfil)
+                        .into(binding.imgPerfil)
+                }catch (e : Exception)
+                {
+
+                }
+            }else{
+                Toast.makeText(
+                    this,
+                    "Cancelado",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+    private val concederPermisoAlmacenamiento =
+        registerForActivityResult(ActivityResultContracts.RequestPermission())
+        {
+            concesion ->
+            if(concesion)
+            {
+                imagenGaleria()
+            }else{
+                Toast.makeText(
+                    this,
+                    "El permiso de almacenamiento ha sido denegado",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+    private fun imagenGaleria() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        resultadoGaleria_ARL.launch(intent)
+    }
+
+    private val resultadoGaleria_ARL =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+        {
+            resultado ->
+            if(resultado.resultCode == Activity.RESULT_OK)
+            {
+                val data = resultado.data
+                imageUri = data!!.data
+
+                try{
+                    Glide.with(this)
+                        .load(imageUri)
+                        .placeholder(R.drawable.img_perfil)
+                        .into(binding.imgPerfil)
+                }catch (e : Exception)
+                {
+
+                }
+            }else{
+                Toast.makeText(
+                    this,
+                    "Cancelado",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
 }
