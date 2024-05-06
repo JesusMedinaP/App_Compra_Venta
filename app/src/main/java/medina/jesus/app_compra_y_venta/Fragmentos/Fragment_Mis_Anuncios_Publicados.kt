4ce2f387b2982fa901db9eda8845b2baf971f30e
JesusMedinaP <jesusmedinaparraga@gmail.com>
2024-05-06 19:59:28 +0200

    Tercera parte de Mis Anuncios publicados. Buscar un anuncio. Implementada la misma lógica anterior para la barra de búsqueda y filtrar los resultados.

--- app/src/main/java/medina/jesus/app_compra_y_venta/Fragmentos/Fragment_Mis_Anuncios_Publicados.kt
+++ app/src/main/java/medina/jesus/app_compra_y_venta/Fragmentos/Fragment_Mis_Anuncios_Publicados.kt
@@ -2,10 +2,14 @@ package medina.jesus.app_compra_y_venta.Fragmentos
 
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -40,6 +44,35 @@ class Fragment_Mis_Anuncios_Publicados : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         firebaseAuth = FirebaseAuth.getInstance()
         cargarMisAnuncios()
+        binding.EtBuscar.addTextChangedListener (object: TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(filtro: CharSequence?, start: Int, before: Int, count: Int) {
+                try {
+                    val consulta = filtro.toString()
+                    anunciosAdaptador.filter.filter(consulta)
+                }catch (e:Exception){
+                    Constantes.toastConMensaje(contexto, "Algo ha ido mal al buscar los anuncios")
+                    println(e.message)
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        })
+
+        binding.IbLimpiar.setOnClickListener {
+            val consulta = binding.EtBuscar.text.toString().trim()
+            if(consulta.isNotEmpty()){
+                binding.EtBuscar.setText("")
+                Constantes.toastConMensaje(contexto, "Se ha limpiado el campo de búsqueda")
+            }else{
+                Constantes.toastConMensaje(contexto, "No se ha ingresado una consulta")
+            }
+        }
     }
 
     private fun cargarMisAnuncios() {
