3bec58269d7b28762bfd181be1f4feea7eade25a
JesusMedinaP <jesusmedinaparraga@gmail.com>
2024-05-10 13:17:21 +0200

    Quinta parte de Comentarios del usuario hacia el vendedor. Implementada la funcionalidad para poder borrar comentarios propios en la sección de comentarios

--- app/src/main/java/medina/jesus/app_compra_y_venta/Adaptadores/AdaptadorComentario.kt
+++ app/src/main/java/medina/jesus/app_compra_y_venta/Adaptadores/AdaptadorComentario.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
@@ -19,10 +21,12 @@ class AdaptadorComentario: RecyclerView.Adapter<AdaptadorComentario.HolderComent
     val comentarios : ArrayList<Comentario>
 
     private lateinit var binding : ItemComentarioBinding
+    private lateinit var firebaseAuth: FirebaseAuth
 
     constructor(context: Context, comentarios: ArrayList<Comentario>) {
         this.context = context
         this.comentarios = comentarios
+        firebaseAuth = FirebaseAuth.getInstance()
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HolderComentario {
@@ -48,6 +52,42 @@ class AdaptadorComentario: RecyclerView.Adapter<AdaptadorComentario.HolderComent
         holder.Tv_comentario.text = comentario
 
         cargarInformacion(modelo, holder)
+
+        holder.itemView.setOnClickListener {
+            dialogEliminarComentario(modelo, holder)
+        }
+    }
+
+    private fun dialogEliminarComentario(
+        modelo: Comentario,
+        holder: HolderComentario
+    ) {
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle("Eliminar comentario")
+        builder.setMessage("¿Estás seguro de querer eliminar este comentario?")
+            .setPositiveButton("Eliminar"){d, e ->
+                val uidUsuario = modelo.uid
+                if(firebaseAuth.uid.equals(uidUsuario)){
+                    val uidVendedor = modelo.uidVendedor
+                    val id = modelo.id
+                    val ref = Constantes.obtenerReferenciaComentariosDB()
+                    ref.child(uidVendedor).child("Comentarios").child(id)
+                        .removeValue()
+                        .addOnSuccessListener {
+                            Constantes.toastConMensaje(context, "Se ha eliminado el comentario")
+                        }
+                        .addOnFailureListener { e->
+                            Constantes.toastConMensaje(context, "No se ha podido eliminar el comentario")
+                            println(e.message)
+                        }
+                }else{
+                    Constantes.toastConMensaje(context, "No puedes eliminar el comentario de otro usuario")
+                }
+            }
+            .setNegativeButton("Cancelar"){d, e ->
+                d.dismiss()
+            }
+            .show()
     }
 
     private fun cargarInformacion(
