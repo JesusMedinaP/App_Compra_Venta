8044df0bf3b5984037e8c3f6a2fe3ab75a4fbd9f
JesusMedinaP <jesusmedinaparraga@gmail.com>
2024-05-11 12:47:15 +0200

    Tercera parte Chat en tiempo real. Información vendedor. Implementada la lógica para cargar la información que se presenta en el chat del vendedor

--- app/src/main/java/medina/jesus/app_compra_y_venta/Chat/Chat.kt
+++ app/src/main/java/medina/jesus/app_compra_y_venta/Chat/Chat.kt
@@ -4,8 +4,13 @@ import android.app.ProgressDialog
 import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import medina.jesus.app_compra_y_venta.Constantes
+import medina.jesus.app_compra_y_venta.R
 import medina.jesus.app_compra_y_venta.databinding.ActivityChatBinding
 
 class Chat : AppCompatActivity() {
@@ -40,5 +45,43 @@ class Chat : AppCompatActivity() {
         binding.icRegresar.setOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
+
+        cargarInfoVendedor()
+    }
+
+    private fun cargarInfoVendedor()
+    {
+        val ref = Constantes.obtenerReferenciaUsuariosDB()
+        ref.child(uidVendedor)
+            .addValueEventListener(object : ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    try {
+                        val nombre = "${snapshot.child("nombres").value}"
+                        val imagen = "${snapshot.child("urlImagenPerfil").value}"
+
+                        binding.TxtNombreVendedorChat.text = nombre
+                        try {
+
+                            Glide.with(this@Chat)
+                                .load(imagen)
+                                .placeholder(R.drawable.img_perfil)
+                                .into(binding.ToolbarIv)
+
+                        }catch (e : Exception){
+                            println("Error al cargar la imagne con Glide " + e.message)
+                        }
+                    }catch (e : Exception){
+                        println("Error al acceder al vendedor " + e.message)
+                        Constantes.toastConMensaje(this@Chat, "Ha habido un problema al cargar la info " +
+                                "del vendedor")
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    println("Error en la base de datos " + error.message)
+                    Constantes.toastConMensaje(this@Chat, "Ha habido un problema al acceder a la info " +
+                            "del vendedor")
+                }
+            })
     }
 }
