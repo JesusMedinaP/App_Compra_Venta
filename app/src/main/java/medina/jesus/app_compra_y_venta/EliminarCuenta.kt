eee7d97142c382bbd551c3ad8ef5578b94dad8b0
JesusMedinaP <jesusmedinaparraga@gmail.com>
2024-05-12 21:20:30 +0200

    Añadido el feedback para la comprobación de seguridad al eliminar la cuenta de usuario

--- app/src/main/java/medina/jesus/app_compra_y_venta/EliminarCuenta.kt
+++ app/src/main/java/medina/jesus/app_compra_y_venta/EliminarCuenta.kt
@@ -6,6 +6,7 @@ import android.app.ProgressDialog
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DataSnapshot
@@ -21,6 +22,7 @@ class EliminarCuenta : AppCompatActivity() {
     private lateinit var progressDialog: AlertDialog
     private lateinit var firebaseAuth: FirebaseAuth
     private var firebaseUser : FirebaseUser?=null
+    private var intentos = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEliminarCuentaBinding.inflate(layoutInflater)
@@ -41,49 +43,70 @@ class EliminarCuenta : AppCompatActivity() {
     }
 
     private fun eliminarCuenta() {
-        progressDialog = SpotsDialog.Builder()
-            .setContext(this)
-            .setMessage("Eliminando su cuenta")
-            .setCancelable(false)
-            .build()
-            .apply { show() }
+        if(intentos == 0)
+        {
+            val alertDialog = MaterialAlertDialogBuilder(this)
+            alertDialog.setTitle("Aviso eliminar cuenta")
+                .setMessage("Para poder eliminar tu cuenta permanentemente debes cerrar e " +
+                        "iniciar sesión como modo de confirmación para esta operación. Después " +
+                        "podrás eliminar la cuenta.")
+                .setNeutralButton("Aceptar"){ dialog, which ->
+                    dialog.dismiss()
+                }.show()
+            intentos++
+        }else {
+            progressDialog = SpotsDialog.Builder()
+                .setContext(this)
+                .setMessage("Eliminando su cuenta")
+                .setCancelable(false)
+                .build()
+                .apply { show() }
 
-        val uidUsuario = firebaseAuth.uid
-        firebaseUser!!.delete()
-            .addOnSuccessListener {
-                val anuncios = Constantes.obtenerReferenciaAnunciosDB()
-                anuncios.orderByChild("uid").equalTo(uidUsuario)
-                    .addValueEventListener(object : ValueEventListener{
-                        override fun onDataChange(snapshot: DataSnapshot) {
-                            for(ds in snapshot.children){
-                                ds.ref.removeValue()
+            val uidUsuario = firebaseAuth.uid
+            firebaseUser!!.delete()
+                .addOnSuccessListener {
+                    val anuncios = Constantes.obtenerReferenciaAnunciosDB()
+                    anuncios.orderByChild("uid").equalTo(uidUsuario)
+                        .addValueEventListener(object : ValueEventListener {
+                            override fun onDataChange(snapshot: DataSnapshot) {
+                                for (ds in snapshot.children) {
+                                    ds.ref.removeValue()
+                                }
+                                Constantes.obtenerReferenciaComentariosDB().child(uidUsuario!!)
+                                    .removeValue()
+                                Constantes.obtenerReferenciaUsuariosDB().child(uidUsuario!!)
+                                    .removeValue()
+                                    .addOnSuccessListener {
+                                        progressDialog.dismiss()
+                                        irMainActivity()
+                                    }
+                                    .addOnFailureListener { e ->
+                                        progressDialog.dismiss()
+                                        Constantes.toastConMensaje(
+                                            this@EliminarCuenta,
+                                            "Ha habido un problema al eliminar el usuario"
+                                        )
+                                        println(e.message)
+                                        irMainActivity()
+                                    }
                             }
-                            Constantes.obtenerReferenciaComentariosDB().child(uidUsuario!!).removeValue()
-                            Constantes.obtenerReferenciaUsuariosDB().child(uidUsuario!!).removeValue()
-                                .addOnSuccessListener {
-                                    progressDialog.dismiss()
-                                    irMainActivity()
-                                }
-                                .addOnFailureListener { e->
-                                    progressDialog.dismiss()
-                                    Constantes.toastConMensaje(this@EliminarCuenta, "Ha habido un problema al eliminar el usuario")
-                                    println(e.message)
-                                    irMainActivity()
-                                }
-                        }
 
-                        override fun onCancelled(error: DatabaseError) {
-                            progressDialog.dismiss()
-                            println(error.message)
-                            Constantes.toastConMensaje(this@EliminarCuenta, "Ha habido un problema al eliminar sus anuncios")
-                        }
-                    })
-            }
-            .addOnFailureListener {e->
-                progressDialog.dismiss()
-                Constantes.toastConMensaje(this, "Ha habido un problema al eliminar la cuenta")
-                println(e.message)
-            }
+                            override fun onCancelled(error: DatabaseError) {
+                                progressDialog.dismiss()
+                                println(error.message)
+                                Constantes.toastConMensaje(
+                                    this@EliminarCuenta,
+                                    "Ha habido un problema al eliminar sus anuncios"
+                                )
+                            }
+                        })
+                }
+                .addOnFailureListener { e ->
+                    progressDialog.dismiss()
+                    Constantes.toastConMensaje(this, "Ha habido un problema al eliminar la cuenta")
+                    println(e.message)
+                }
+        }
     }
 
     private fun irMainActivity()
