b44fd7655a5eb26597f17d95fd0f949467bc2622
JesusMedinaP <jesusmedinaparraga@gmail.com>
2024-05-13 10:57:01 +0200

    Mejoras propuestas por mi jefa de reformas

--- app/src/main/java/medina/jesus/app_compra_y_venta/DetalleVendedor/DetalleVendedor.kt
+++ app/src/main/java/medina/jesus/app_compra_y_venta/DetalleVendedor/DetalleVendedor.kt
@@ -2,6 +2,7 @@ package medina.jesus.app_compra_y_venta.DetalleVendedor
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.google.firebase.database.DataSnapshot
@@ -62,6 +63,11 @@ class DetalleVendedor : AppCompatActivity() {
                     val contadorAnuncios = "${anunciosArrayList.size}"
                     binding.TvNumAnuncios.text = contadorAnuncios
 
+                    if(anunciosArrayList.isEmpty()){
+                        binding.TvFeedback.visibility = View.VISIBLE
+                    }else{
+                        binding.TvFeedback.visibility = View.GONE
+                    }
                 }
 
                 override fun onCancelled(error: DatabaseError) {
